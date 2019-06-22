5d3e737895c8295fbf22115a40aa2fb393016b24
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-06-22 19:17:31 +0300

    Set up window creation and destruction

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main() {
 
-    std::cout << "Hello, thanks for playing my game!" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "ERROR::Failed to initialize SDL!" << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Ping Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == NULL) {
+        std::cerr << "ERROR::Failed to create window!" << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
