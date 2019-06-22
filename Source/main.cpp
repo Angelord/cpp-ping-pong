90668351fdb891ea66769a1157c25030cb94970a
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-06-22 19:31:48 +0300

    Add event handling

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,25 +4,63 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+SDL_Window* g_window = NULL;
+SDL_Surface* g_screenSurface = NULL;
+bool g_gameOver = false;
+
+bool Initialize();
+
+void Shutdown();
+
+void HandleEvents();
+
 int main() {
+
+    if(!Initialize()) {
+        return -1;
+    }
+
+    while(!g_gameOver) {
+
+        HandleEvents();
+    }
+
+    Shutdown();
+
+    return 0;
+}
+
+bool Initialize() {
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "ERROR::Failed to initialize SDL!" << SDL_GetError() << std::endl;
-        return -1;
+        return false;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Ping Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if(window == NULL) {
+    g_window = SDL_CreateWindow("Ping Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(g_window == NULL) {
         std::cerr << "ERROR::Failed to create window!" << SDL_GetError() << std::endl;
-        return -1;
+        return false;
     }
 
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    g_screenSurface = SDL_GetWindowSurface(g_window);
 
-    SDL_Delay(3000);
+    return true;
+}
 
-    SDL_DestroyWindow(window);
+void Shutdown() {
+
+    SDL_DestroyWindow(g_window);
+
     SDL_Quit();
+}
 
-    return 0;
+void HandleEvents() {
+    static SDL_Event ev;
+
+    while(SDL_PollEvent(&ev) != 0) {
+        if(ev.type == SDL_QUIT) {
+            g_gameOver = true;
+        }
+    }
 }
